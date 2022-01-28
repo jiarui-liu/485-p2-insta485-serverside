d7c45c2afe6aa390fe5c278ab8f4ff59507721fa
Jiarui Liu <ljrjerry@umich.edu>
2022-01-28 14:03:19 -0500

    finish database

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,46 +6,42 @@ CREATE TABLE users(
     email VARCHAR(40) NOT NULL,
     filename VARCHAR(64) NOT NULL,
     password VARCHAR(256) NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
     PRIMARY KEY(username)
 );
 
 CREATE TABLE posts(
-    postid int NOT NULL AUTO_INCREMENT,
+    postid INTEGER PRIMARY KEY AUTOINCREMENT,
     filename VARCHAR(64) NOT NULL,
     owner VARCHAR(20) NOT NULL,
-    FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(postid)
-
+    created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE
 );
 
 CREATE TABLE following(
     username1 VARCHAR(20) NOT NULL,
-    FOREIGN KEY(username1) REFERENCES users(username) ON DELETE CASCADE,
     username2 VARCHAR(20) NOT NULL,
+    created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    FOREIGN KEY(username1) REFERENCES users(username) ON DELETE CASCADE,
     FOREIGN KEY(username2) REFERENCES users(username) ON DELETE CASCADE,
-    PRIMARY KEY(username1, username2),
-    created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    PRIMARY KEY(username1, username2)
 );
 
 CREATE TABLE comments(
-    commentid int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(commentid),
+    commentid INTEGER PRIMARY KEY AUTOINCREMENT,
     owner VARCHAR(20) NOT NULL,
-    FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
-    postid int NOT NULL,
-    FOREIGN KEY(postid) REFERENCES posts(postid),
+    postid INT NOT NULL,
     text VARCHAR(1024) NOT NULL,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
+    FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
+    FOREIGN KEY(postid) REFERENCES posts(postid)
 );
 
 CREATE TABLE likes(
-    likeid int NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY(likeid),
+    likeid INTEGER PRIMARY KEY AUTOINCREMENT,
     owner VARCHAR(20) NOT NULL,
+    postid INT NOT NULL,
+    created DATETIME NOT NULL DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')),
     FOREIGN KEY(owner) REFERENCES users(username) ON DELETE CASCADE,
-    postid int NOT NULL,
-    FOREIGN KEY(postid) REFERENCES posts(postid) ON DELETE CASCADE,
-    created DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    FOREIGN KEY(postid) REFERENCES posts(postid) ON DELETE CASCADE
 )
